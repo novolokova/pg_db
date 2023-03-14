4bcba695cc101ab0ecf358c068203222540a0c9c
Margarita <m.novolokova@gmail.com>
2023-03-14 21:27:40 +0200

    Refactor hw-14-03

--- sql/hw-14-03.sql
+++ sql/hw-14-03.sql
@@ -1,5 +1,8 @@
 -- порахувати люди якого сезону народження витрачають найбільше грошей
-SELECT sum("oc"."cost"),  "es_bd"."season"
+
+SELECT sum("oc"."cost") AS "costs_season",  "es_bd"."season"
 FROM "order_cost" AS "oc"
 JOIN "email_seasonBD" AS "es_bd" ON "oc"."userId" = "es_bd"."id"
-GROUP BY "es_bd"."season";
+GROUP BY "es_bd"."season"
+ORDER BY  "costs_season" DESC
+LIMIT 1;
